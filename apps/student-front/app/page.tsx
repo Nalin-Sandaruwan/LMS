53d025ed2bd13b8d109ad58e82644ce4f7e0a71d
Nalin-Sandaruwan <creationbynalin@gmail.com>
2026-05-04 01:06:29 +0530

    feat: implement student landing page with hero, testimonial, and interactive UI components

--- apps/student-front/app/page.tsx
+++ apps/student-front/app/page.tsx
@@ -2,6 +2,7 @@ import { Hero } from "@/components/base compo/hero";
 import { Navigation } from "@/components/base compo/navigation";
 import { SectionSec } from "@/components/base compo/sectionSec";
 import { Testimonials } from "@/components/base compo/testamonial";
+import { VibeSection } from "@/components/base compo/vibeSection";
 import { Footer } from "@/components/base compo/footer";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/dark.mood";
@@ -13,6 +14,7 @@ export default function Home() {
       <Navigation />
       <main className="grow">
         <Hero />
+        <VibeSection />
         <SectionSec />
         <Testimonials />
       </main>
