7fa281522a429d1f6fba50f416af576b1e0b9561
Nalin-Sandaruwan <creationbynalin@gmail.com>
2026-05-10 00:08:03 +0530

    test: add unit tests for CourseController and SectionService

--- apps/lms-service/src/section/section.service.spec.ts
+++ apps/lms-service/src/section/section.service.spec.ts
@@ -3,6 +3,7 @@ import { SectionService } from './section.service';
 
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Section } from './entities/section.entity';
+import { BunnyStreamService } from '../lessons/bunny-stream.service';
 
 describe('SectionService', () => {
   let service: SectionService;
@@ -11,7 +12,8 @@ describe('SectionService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         SectionService,
-        { provide: getRepositoryToken(Section), useValue: {} }
+        { provide: getRepositoryToken(Section), useValue: {} },
+        { provide: BunnyStreamService, useValue: {} },
       ],
     }).compile();
 
